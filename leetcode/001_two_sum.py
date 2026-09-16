61d11820b56162f35c503e4d678c289fcbd5bd81
Rahul degra <rahuldegar28@gmail.com>
2026-09-16 16:12:50 +0530

    Add valid parentheses stack solution and linked list

--- leetcode/001_two_sum.py
+++ leetcode/001_two_sum.py
@@ -1,12 +1,8 @@
-
-def twoSum(nums, target):
-    history = {}
-    for index, num in enumerate(nums):
-        needed = target - num
-        if needed in history:
-            return [history[needed], index]
-        history[num] = index
-        
-print(twoSum([2, 7, 11, 15], 9)) # Expected: [0, 1]
-print(twoSum([3, 2, 4], 6))      # Expected: [1, 2]
-print(twoSum([3, 3], 6))         # Expected: [0, 1]
+class Solution(object):
+    def twoSum(self, nums, target):
+        history = {}
+        for index, num in enumerate(nums):
+            needed = target - num
+            if needed in history:
+                return [history[needed], index]
+            history[num] = index
